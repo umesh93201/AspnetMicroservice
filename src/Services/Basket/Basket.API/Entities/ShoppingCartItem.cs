94332b29d54995d285c04b6967cd083abfad558b
Sethi, Umeshnath <Umeshnath_Sethi@Dell.com>
2021-06-19 06:23:27 +0530

    Adding Basket API code

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+
+        public string Color { get; set; }
+
+        public string ProductID { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ProductName { get; set; }
+
+    }
+}
